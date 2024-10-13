61933610fee1a7211336471a955f3158c9129f7e
Jonathan Alonso <jonathanalonsom@outlook.com>
2024-10-13 06:01:22 -0300

    terminando de criar Amb Virtual

--- secao3/2_Intemediario/aula132_criandoAmbVirt.py
+++ secao3/2_Intemediario/aula132_criandoAmbVirt.py
@@ -1,4 +1,9 @@
 '''
+
+PARA CRIAR O AMBIENTE VIERTUAL USE:
+
+python -m venv venv
+
 para ativar o ambiente virtual: venv\Scripts\activate
 para desativar o ambiente virtual: deactivate
 
@@ -14,9 +19,26 @@ pip freeze mostra o q vc tem instalado na maquina virtual
 
 se vc tiver num projeto e adc varios modulos e precisa das versoes deles no futuro
 voce pode, ao inves de enviar tudo pro repositorio (git), que vai ficar mt pesado,
-pode criar o arquivo "REQUIRIMENTS", onde vai salvar apenas as versoes dos modulos em um txt
+pode criar o arquivo "REQUIReMENTS", onde vai salvar apenas as versoes dos modulos em um txt
 assim, preservando as versoes.
 
 qd quiser basta chamar:
 pip install [modulox]==[numero da versao.]
+
+para criar esse arquivo txt, basta usar o COMANDO:
+
+pip freeze > requirements.txt #freeze serve pra explicitar as versoes de todos
+os modulos externos q vc ta utilizando.
+
+PARA CHAMAR DE VOLTA O AMBIENTE VIRTUAL JA CRIADO
+ATRAVES DO REQUIRIMENTS.TXT basta usar:
+
+pip install -r .\requirements.txt
+"-r de requirements"
+
+SE QUISER ATUALIZAR AS VERSOES DOS MODULOS BASTA:
+
+python -m pip install pip --upgrade
+
+
 '''
