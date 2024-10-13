477db2269bd46a3b1afae087dbb3947922d54e8c
Jonathan Alonso <jonathanalonsom@outlook.com>
2024-10-13 06:46:44 -0300

    abrir, ler, escrever arquivos

--- secao3/2_Intemediario/aula133_criandoArquivs.py
+++ secao3/2_Intemediario/aula133_criandoArquivs.py
@@ -19,7 +19,7 @@
 # json.dump = Gera um arquivo json
 # json.load
 
-caminho_arquivo = "C:\\temp\\ws-Python\\" # ou vc inverte as barras ou poe 2 barras, normalmente colocam 2.
+caminho_arquivo = r"C:\\temp\\ws-Python\\" # ou vc inverte as barras ou poe 2 barras, normalmente colocam 2.
 caminho_arquivo += "aula116.txt"
 
 # arquivo = open(caminho_arquivo, 'w')
@@ -31,8 +31,31 @@ caminho_arquivo += "aula116.txt"
 # arquivo.close()
 
 
-#Uma forma de abrir e fechar automaticamente, é usando o "with open"
+#(context manager)Uma forma de abrir e fechar automaticamente, é usando o "with open"
 
-with open(caminho_arquivo, 'w') as arquivo:
-    print('ola mundo')
-    print('Arquvio vai ser fechado')
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+         ) # mais usado para escrever ITERAVEIS.
+    # arquivo.seek(0,0) #mover pro topo do arquivo, nao se utiliza mt.
+    print(arquivo.read())
+    arquivo.seek(0, 0)
+    print(arquivo.readline(), end='') # para o print n pular linha 
+    print(arquivo.readline().strip()) #retorna sem espaços e quebras de linha do texto.
+    print(arquivo.readline().strip())
+    print(arquivo.readline().strip())
+    print('READLINES')
+    arquivo.seek(0,0)
+    for linha in arquivo.readlines():
+        print(linha.strip()) #tirar a quebra de linha a mais.
+
+print('#'* 10)
+
+with open(caminho_arquivo, 'r') as arquivo:
+    print(arquivo.read())
+    #Linha 1
+    #Linha 1
+    print('lendo')
+    
