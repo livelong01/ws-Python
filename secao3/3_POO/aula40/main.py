8c08a6b6da98af70663500ef40cab5dfb089407b
Jonathan Alonso <jonathanalonsom@outlook.com>
2024-11-23 11:30:36 -0300

    help, dir e DocString 1 linha

---
+++ secao3/3_POO/aula40/main.py
@@ -0,0 +1,10 @@
+import uma_linha
+
+print(dir(uma_linha)) #mostra tudo que tem dentor do seu modulo
+print(uma_linha.__doc__)
+print(uma_linha.__file__) #returna o caminho do arquivo
+print(uma_linha.__doc__)
+
+help(uma_linha) 
+#o HELP mostra de forma mais amigavel tudo q tem no modulo.
+
---
+++ secao3/3_POO/aula40/uma_linha.py
@@ -0,0 +1,9 @@
+"""
+O que seu módulo faz: #exemplo de DocString
+"""
+
+
+variavel = 'valor'
+
+def funcao():
+    return 1
