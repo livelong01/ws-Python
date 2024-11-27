5f4147c37cf2e93aeb6bd840ff8901fd97a43230
Jonathan Alonso <jonathanalonsom@outlook.com>
2024-11-27 12:36:18 -0300

    Documentacao, como fazer

--- secao3/3_POO/aula40/main.py
+++ secao3/3_POO/aula40/main.py
@@ -1,10 +1,10 @@
-import varias_linhas
-
+# import varias_linhas
+import aula_41DocString_funcao
 # print(dir(uma_linha)) #mostra tudo que tem dentor do seu modulo
 # print(uma_linha.__doc__)
 # print(uma_linha.__file__) #returna o caminho do arquivo
 # print(uma_linha.__doc__)
 
-help(varias_linhas) 
+help(aula_41DocString_funcao) 
 #o HELP mostra de forma mais amigavel tudo q tem no modulo.
 
