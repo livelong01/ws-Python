5366f8e94bd2fe4e63a900091895771da1148493
Jonathan Alonso <jonathanalonsom@outlook.com>
2025-10-03 12:08:42 +0100

    criando a tabela com o mysql

--- secao3/8_SQL_Com_Python/MYSQL/main.py
+++ secao3/8_SQL_Com_Python/MYSQL/main.py
@@ -2,3 +2,29 @@
 # Doc: https://pymysql.readthedocs.io/en/latest/
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
+
+import os
+import pymysql
+import dotenv
+
+dotenv.load_dotenv()
+
+connection = pymysql.connect(
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
+)
+
+with connection:
+    with connection.cursor() as cursor:
+        #  SQL
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS customers ('
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'nome VARCHAR(50) NOT NULL, '
+            'idade INT NOT NULL, '
+            'PRIMARY KEY (id) '
+            ') '
+        )
+        connection.commit()
