54a3719c9be336e89f214118026fbf98e6975344
Jonathan Alonso <jonathanalonsom@outlook.com>
2025-10-14 12:03:56 +0100

    Lendo tabelas com my sql

---
+++ secao3/Exercicios/tabuada.py
@@ -0,0 +1,26 @@
+
+# Tabuada:
+
+num = int(input("Digite um número para ver a tabuada: "))
+print(f"Tabuada do {num}:")
+for i in range(1, 11):
+    resultado = num * i
+    print(f"{num} x {i} = {resultado}")
+
+
+
+'''
+num : inteiro 
+lista : lista
+resultado : inteiro
+
+lista = (1,2,3,4,5,6,7,8,9,10)
+
+num = escreval("Digite um numero para ver a tabuada: ")
+leia("Tabuada do " + num + ": ")
+para i na lista:
+    resultado = num * i
+    leia(num + " x " + i + " = " + resultado)
+
+
+'''
